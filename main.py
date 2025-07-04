31edcb29f8e7aa57cd1c58a738d61a1070c6b83c
Guadalupe Quintal V <gquintalv@protonmail.com>
2025-07-04 02:39:24 -0600

    Se agrega main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+import logging
+import os
+import sys
+from dotenv import load_dotenv
+from etl import Extractor, Transformer, Loader
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+            logging.FileHandler('etl.log')
+        ]
+    )
+
+
+def main():
+
+    setup_logging()
+    logger = logging.getLogger(__name__)
+
+    load_dotenv()
+
+    # Get environment variables
+    url = os.getenv('USERS_API_URL')
+    count = os.getenv('USERS_COUNT', '100')
+
+    logger.info("Iniciando el proceso ETL")
+
+    try:
+        # Vakidate URL
+        if not url.startswith('http'):
+            raise ValueError("La URL debe comenzar con 'http' o 'https'")
+        logging.info("URL de la API es válida")
+    
+    except ValueError as ve:
+        logging.error(f"Error de validación de URL: {ve}")
+        return
+    
+    try:
+        #Extract
+        logger.info("Starting data extraction")
+        extractor = Extractor()
+        raw_data = extractor.extract(url,count)
+        
+        if not raw_data:
+            logging.error("No data extracted, exiting ETL process")
+            return False
+        
+        #Transform
+        logger.info("Starting data transformation")
+        transformer = Transformer()
+        users_transformed = transformer.transform(raw_data)
+
+        if not users_transformed:
+            logging.error("No data transformed, exiting ETL process")
+            return False
+                
+        #Load
+        logger.info("Starting data loading")
+        loader = Loader()
+        loader.load(users_transformed)
+
+        if loader:
+            logger.info("Data loaded successfully")
+            return True
+        else:
+            logger.error("Data loading failed")
+            return False
+
+    except Exception as e:
+        logging.error(f"An error occurred during the ETL process: {e}")
+        return False
+    
+if __name__ == "__main__":
+    main()
+    logging.info("ETL process completed")
